9bda2762866e079f58abbf088eba2fa7c690f6a3
BlueCannonBall <mmttiizz100@gmail.com>
2022-06-20 14:01:24 -0400

    mimetypes

--- api/main.cpp
+++ api/main.cpp
@@ -1,18 +1,15 @@
 #include "Polyweb/polyweb.hpp"
 
 int main() {
+    pn::init();
+
     pw::Server server;
     server.bind("0.0.0.0", 8000);
     server.route("/", [](pw::Connection& conn, const pw::HTTPRequest& req) {
-        auto data = req.build();
-        std::cout << std::string(data.begin(), data.end()) << std::endl;
-        pw::HTTPResponse resp;
-        resp.status_code = "200";
-        resp.reason_phrase = "OK";
-        resp.headers["content-type"] = "text/html";
-        std::string resp_string = "<html><body><h1>hello</h1></body></html>";
-        resp.body = std::vector<char>(resp_string.begin(), resp_string.end());
+        pw::HTTPResponse resp("200", "<html><body><h1>Hello, World!</h1></body></html>", {{"Content-Type", pw::filename_to_mimetype("html")}});
         return resp;
     });
     server.listen(128);
+
+    return pn::quit();
 }
