e5239eea1dcd8c4a6c1af0d2f51560cd1520a262
BlueCannonBall <mmttiizz100@gmail.com>
2022-06-21 21:47:48 -0400

    various fixes

--- api/main.cpp
+++ api/main.cpp
@@ -1,18 +1,23 @@
 #include "Polyweb/polyweb.hpp"
 #include <fstream>
+#include <signal.h>
 
 int main() {
     pn::init();
+    signal(SIGPIPE, SIG_IGN);
 
     pw::Server server;
-    server.bind("0.0.0.0", 5000);
+    if (server.bind("0.0.0.0", 5000) == PW_ERROR) {
+        std::cerr << pw::universal_strerror(pw::get_last_error()) << std::endl;
+        return 1;
+    }
 
     server.route("/*", [](pw::Connection& conn, const pw::HTTPRequest& req) {
         std:: cout << "Got request:\n" << req.build_str() << std::endl;
 
         std::ifstream file("./" + req.target);
         if (!file.is_open()) {
-            return pw::HTTPResponse("500", "Error opening file\n", {{"Content-Type", "text/plain"}});
+            return pw::HTTPResponse("404", "Error opening file\n", {{"Content-Type", "text/plain"}});
         }
 
         try {
@@ -23,10 +28,30 @@ int main() {
         } catch (const std::ios::failure& e) {
             return pw::HTTPResponse("500", "Error reading file\n", {{"Content-Type", "text/plain"}});
         }
-
     });
 
-    server.listen(128);
+    server.route("/", [](pw::Connection& conn, const pw::HTTPRequest& req) {
+        std:: cout << "Got request:\n" << req.build_str() << std::endl;
+
+        std::ifstream file("index.html");
+        if (!file.is_open()) {
+            return pw::HTTPResponse("404", "Error opening file\n", {{"Content-Type", "text/plain"}});
+        }
+
+        try {
+            std::string str((std::istreambuf_iterator<char>(file)),
+                std::istreambuf_iterator<char>());
+
+            return pw::HTTPResponse("200", str, {{"Content-Type", "text/html"}});
+        } catch (const std::ios::failure& e) {
+            return pw::HTTPResponse("500", "Error reading file\n", {{"Content-Type", "text/plain"}});
+        }
+    });
+
+    if (server.listen(128) == PW_ERROR) {
+        std::cerr << pw::universal_strerror(pw::get_last_error()) << std::endl;
+        return 1;
+    }
 
     return pn::quit();
 }
